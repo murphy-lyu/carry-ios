2e6f8602242c4474d69322e447e2ac1ad2a1e00c
Murphy <murphy.lyu@icloud.com>
2026-05-14 10:49:07 +0800

    Initial Commit

---
+++ Carry/CarryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarryApp.swift
+//  Carry
+//
+//  Created by shmilvtian5 on 2026/5/14.
+//
+
+import SwiftUI
+
+@main
+struct CarryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Carry
+//
+//  Created by shmilvtian5 on 2026/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
